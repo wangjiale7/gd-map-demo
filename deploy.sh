4d3a9f45162b81217325f0419a0ae7841e79d9a5
wangjiale <1471777367@qq.com>
2022-11-23 09:57:03 +0800

    gh-pages

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-npm run build 
-scp -r /Users/jiale/development/front/map-vue-demo/dist/* root@114.116.110.47:/root/nginx/html
--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const securityJsCode = 'e163454dac78935602b053f3415866b8'
-export const AmapKey = '7ef64f98637d8b301299b289471ada42'
